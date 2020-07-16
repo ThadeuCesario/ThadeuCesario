815fbae3b3616823b0d81ee3a51b5e72ab3981da
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-07-16 14:45:23 -0300

    Using stylesheets

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 
 import PrimeiroComponente from './components/Primeiro';
 import {MultiComponenteOne, MultiComponenteTwo, MultiComponenteThree as AnoterName} from './components/Multi';
 
 export default () => {
     return(
-        <View>
-            <Text>{2+2}</Text>
+        <View style={style.App}>
             <PrimeiroComponente></PrimeiroComponente>
             <MultiComponenteOne />
             <MultiComponenteTwo />
@@ -15,3 +14,10 @@ export default () => {
         </View>
     )
 }
+
+
+const style = StyleSheet.create({
+    App:{
+        
+    }
+});
