63d131c9e06ae922e8624dc72434774fdb0ea9c3
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-07-16 14:35:37 -0300

    Export methods

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 import PrimeiroComponente from './components/Primeiro';
+import {MultiComponenteOne, MultiComponenteTwo, MultiComponenteThree} from './components/Multi';
 
 export default () => {
     return(
         <View>
             <Text>{2+2}</Text>
             <PrimeiroComponente></PrimeiroComponente>
+            <MultiComponenteOne />
+            <MultiComponenteTwo />
+            <MultiComponenteThree />
         </View>
     )
 }
---
+++ fundamentals/src/components/Multi.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function MultiComponenteOne(){
+    return <Text>Componente 01</Text>
+}
+
+function MultiComponenteTwo(){
+    return <Text>Componente 02</Text>
+}
+
+function MultiComponenteThree(){
+    return <Text>Componente 03</Text>
+}
+
+export {MultiComponenteOne, MultiComponenteTwo, MultiComponenteThree};
