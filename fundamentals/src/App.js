416c3ea29491b55e108086f0f363643d99522a4e
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-07-15 23:08:11 -0300

    Using {} to access javascript

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 
 import PrimeiroComponente from './components/Primeiro';
 
 export default () => {
     return(
         <View>
+            <Text>{2+2}</Text>
             <PrimeiroComponente></PrimeiroComponente>
         </View>
     )
